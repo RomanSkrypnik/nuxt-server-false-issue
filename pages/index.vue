9ffdb5d1e6293a25049e88f8b1638042ad65c7e0
Roman Skrypnik <r.skrypnik@yual.io>
2023-08-18 12:51:26 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    v-if="!pending"
+    v-text="data"
+  />
+  <div
+    v-else
+    v-text="'loading'"
+  />
+</template>
+
+<script setup>
+const { data: cachedData } = useNuxtData('test-key');
+
+const { data, pending } = await useFetch('https://jsonplaceholder.typicode.com/todos/1', {
+  key: 'test-key',
+  server: false,
+});
+</script>
